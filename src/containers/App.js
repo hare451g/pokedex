cff53fef5d282785ffc0f953c6087c8d2ec51564
hare <hendrasadewa30@gmail.com>
2019-07-18 00:52:44 +0700

    Add Router and List Screen Route

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ListScreen from '../screens/ListScreen';
 
-function App(props) {
+function App() {
   return (
-    <div>
-      Pokedex
-    </div>
+    <Router>
+      <Route path="/" component={ListScreen} />
+    </Router>
   );
 }
 
