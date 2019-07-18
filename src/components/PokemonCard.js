bbc6a0e9f97ae24c158bbede1208271d40211399
hare <hendrasadewa30@gmail.com>
2019-07-18 20:20:58 +0700

    Create Pokemon Card to contain pokemon sprites and name

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
+
+export default function PokemonCard({ name, url, id, avatar }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={name}
+          height="140"
+          image={avatar}
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
